dc90d75ee274802c8ffe537a5c7b72375b192a25
DavidzMwangi <mwangidavidwanjohi@gmail.com>
2020-04-01 23:14:15 +0300

    error resolution

--- BloodDonationSystem/urls.py
+++ BloodDonationSystem/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('donator/', include('donator.urls')),
-    path('receiver/', include('receiver.urls') )
+    path('receiver/', include('receiver.urls'))
 ]
