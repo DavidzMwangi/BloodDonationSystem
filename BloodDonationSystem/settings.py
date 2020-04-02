2ff8086f1186c881e3881fb1dbb773c95390e7ec
DavidzMwangi <mwangidavidwanjohi@gmail.com>
2020-04-02 20:15:11 +0300

    chnages

--- BloodDonationSystem/settings.py
+++ BloodDonationSystem/settings.py
@@ -125,3 +125,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 AUTH_USER_MODEL = 'accounts.CustomUser'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
