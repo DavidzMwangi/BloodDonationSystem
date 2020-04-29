d1dbe47c3c442978c0826e2f6a840477a6d43568
DavidzMwangi <mwangidavidwanjohi@gmail.com>
2020-04-29 22:16:09 +0300

    homepage module

--- BloodDonationSystem/urls.py
+++ BloodDonationSystem/urls.py
@@ -5,10 +5,10 @@ The `urlpatterns` list routes URLs to views. For more information please see:
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+    2. Add a URL to urlpatterns:  path('', views.start_page, name='start_page')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='start_page')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
@@ -21,4 +21,5 @@ urlpatterns = [
     path('donator/', include('donator.urls')),
     path('receiver/', include('receiver.urls')),
     path('accounts/', include('accounts.urls')),
+    path('', include('home.urls')),
 ]
